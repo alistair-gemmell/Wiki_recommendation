5aa014928f4fae1509c1190ea9cbb616d72d9c92
alistair-gemmell <gemmala66@gmail.com>
2017-05-27 19:31:56 +0100

    Add files via upload

---
+++ recommendation.py
@@ -0,0 +1,26 @@
+import hdp_model
+
+import logging
+import numpy
+import os.path
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+# Get the topic models for the article list we have on disk
+hdp = hdp_model.return_model(new=False)
+
+# If previous preferences have been saved, load them in, otherwise initialise randomly
+if os.path.isfile('preferences'):
+	preferences = load('preferences')
+else:
+	preferences = numpy.matrix(num_topics)
+
+# Create a shortlist of recommendations, trying to give a selection of different topics
+
+# To Do
+# Other characteristics of articles that might be relevant
+# E.g. Is it a starred article, the length
+
+# Introduce an element of chance, to try and stop a re-inforcmement of previous recommendations
+
+# Produce a recommendation of an article based on previous likes
